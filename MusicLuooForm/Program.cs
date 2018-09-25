f5326c344acdb0ea50dc515ff5abeb9b36626c51
ganyongzai <ganyongzai@tuandai.com>
2018-09-25 17:00:08 +0800

    1

--- MusicLuooForm/Program.cs
+++ MusicLuooForm/Program.cs
@@ -16,7 +16,7 @@ namespace MusicLuoo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLuooPlayer());
+            Application.Run(new FormLuooPlayer()); 
         }
     }
 }
