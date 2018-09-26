3840713389ec0c79316cc8e0e3545e3049767dcc
ganyongzai <ganyongzai@tuandai.com>
2018-09-26 22:07:45 +0800

    1

---
+++ MumiMusic/music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+// 下载于www.mycodes.net
+namespace MumiMusic
+{
+    [Serializable]
+    class music
+    {
+        //获取播放列表
+        public List<string> getList()
+        {
+            return null;
+        }
+        
+    }
+}
